b284b480804d9de7c949b634892be8358103e034
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2024-06-24 17:09:24 +0000

    rendered SettingsPage

--- assignment-5/App.js
+++ assignment-5/App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import SettingsPage from './components/SettingsPage';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <SettingsPage />
       <StatusBar style="auto" />
     </View>
   );
