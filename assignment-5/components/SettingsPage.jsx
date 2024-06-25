06677b5fe04b66c012d2c00a6090467e3dca9410
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2024-06-25 13:22:42 +0000

    added theme text and its icon to switch between colors.

--- assignment-5/components/SettingsPage.jsx
+++ assignment-5/components/SettingsPage.jsx
@@ -1,51 +1,90 @@
-import React from 'react';
-import {View, FlatList, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, FlatList, Text, StyleSheet, Switch, TouchableOpacity} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const settingsData = [
-  {key: 'Language'},
-  {key: "My Profile"},
-  {key: "Contact Us"},
-  {key: "Change Password"},
-  {key: "Privacy Policy", addSeparator: true},
+  {name: 'Language', id: '1'},
+  {name: "My Profile", id: '2'},
+  {name: "Contact Us", id: '3'},
+  {name: "Change Password", id: '4'},
+  {name: "Privacy Policy", id:'5', addSeparator: true},
 ];
 
+const Item = ({name})=> (
+  <View style={settings.item}>
+    <Text style={settings.head}>{name}</Text>
+    <Icon name="chevron-right" size={50} color="gray"/>
+    {/* <Text style={settings.sign}>{'>'}</Text> */}
+  </View>
+);
+
 export default function SettingsPage() {
+  // const renderItem = ({item}) => (
+  //   <View style={settings.item}>
+  //     <Text style={settings.head}>{item.key}</Text>
+  //     {item.addSeparator && <View style={settings.separator}/> }
+  //   </View>
+  // );
+
   const renderItem = ({item}) => (
-    <View style={settings.item}>
-      <Text style={settings.head}>{item.key}</Text>
-      {item.addSeparator && <View style={settings.separator}/> }
-    </View>
+    <>
+    <Item name={item.name} />
+    {item.addSeparator && <View style={settings.separator}/> }
+    </>
   );
 
+  const [isEnabled, setIsEnabled] = useState(false);
+
+  // const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  // // const toggleTheme = () => {
+  // //   setIsDarkTheme(!isDarkTheme);
+  // // }
+
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
   const renderHeader = () => (
-    <View style={settings.header}>
-      <Text 
-        style={settings.headerText}>Settings
-      </Text>
-    </View>
+    <Text 
+      style={settings.headerText}>Settings
+   </Text>
+  )
+
+  const renderFooter = () => (
+    <View style={settings.themeToggle}>
+        <Text style={settings.theme}>Theme</Text>
+        <Switch
+          trackColor={{false: "#767577", true: "lightgreen"}}
+          thumbColor={isEnabled ? "#f4f3f4" : "#f4f3f4"}
+          onValueChange={toggleSwitch}
+          value={isEnabled}
+        />
+      </View>
   )
 
   const renderSeparator = () => (
     <View style={settings.separator}/>
   )
+
+
   return (
-    <FlatList 
-    style={settings.container}
-    data={settingsData}
-    renderItem={renderItem}
-    ItemSeparatorComponent={renderSeparator}
-    ListHeaderComponent={renderHeader}>
-
-      {/* <Text style={settings.header}>Settings</Text>
-      <View style={settings.head}>
-        <Text >Language</Text>
-        <Text>My Profile</Text>
-        <Text>Contact Us</Text>
-        <Text>Change Password</Text>
-        <Text>Privacy Policy</Text>
-
-      </View> */}
-    </FlatList>
+    <View style={settings.container}>
+      <FlatList
+        showsVerticalScrollIndicator={false} 
+        data={settingsData}
+        keyExtractor={item => item.id}
+        renderItem={renderItem}
+        ItemSeparatorComponent={renderSeparator}
+        ListHeaderComponent={renderHeader}
+        ListFooterComponent={renderFooter}>
+      </FlatList>
+      {/* <TouchableOpacity onPress={toggleTheme}>
+          <Icon 
+            name={isDarkTheme ? "weather-night" : "white-balance-sunny"} 
+            size={24} 
+            color={isDarkTheme ? "#FFF" : "#000"} 
+          />
+        </TouchableOpacity> */}
+    </View>
   );
 }
 
@@ -54,34 +93,41 @@ const settings = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     padding: 20,
-    marginTop: 40,
+    marginTop: 50,
   },
   headerText: {
-    fontSize: 30,
+    justifyContent:"center",
+    alignSelf: 'center',
+    fontSize: 35,
     fontWeight: 'bold',
-    marginBottom: 80,
-  },
-  header:{
-    padding: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
+    marginBottom: 70,
   },
   head: {
     fontSize: 25,
-    marginBottom: 15,
-    justifyContent:'flex-start',
-    alignItems: 'flex-start',
-    alignSelf: 'flex-start'
+    marginBottom: 5,
   },
   item: {
-    marginTop: 25,
-    padding:10,
-    alignSelf: 'flex-start'
-
+    marginTop: 30,
+    paddingVertical: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
   },
   separator: {
     height: 1,
+    width: 350,
     backgroundColor: '#CCCCCC',
-    marginLeft: 0,
   },
+  themeToggle: {
+    marginTop: 50,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  theme: {
+    fontSize: 28,
+    marginTop: 40,
+  },
+  sign:{
+    fontSize: 30,
+    color: 'gray'
+  }
 })
