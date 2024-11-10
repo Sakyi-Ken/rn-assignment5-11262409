9a42868e540a0e62cf724305620e1dbb48d90eaa
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2024-11-10 23:43:42 +0000

    Configure bottom-tabs in the App.js

--- assignment-5/App.js
+++ assignment-5/App.js
@@ -1,24 +1,39 @@
 import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
-
+import {Ionicons} from '@expo/vector-icons';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+//import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import SettingsPage from './components/SettingsPage';
 import HomeScreen from './components/HomeScreen';
 
-const Stack = createNativeStackNavigator();
+//const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <View style={styles.container}>
-        <Stack.Navigator initialRouteName='Setting'>
-          <Stack.Screen name='Home' component={HomeScreen}/>
-          <Stack.Screen name="Setting" component={SettingsPage}/>
-        </Stack.Navigator>
+    <NavigationContainer style={styles.container}>
         <StatusBar style="auto" />
-      </View>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+
+              if(route.name === 'Home') {
+                iconName = focused ? 'home': 'home-outline';
+              } else if (route.name === 'Settings') {
+                iconName = focused ? 'settings': 'settings-outline';
+              }
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+            tabBarActiveTintColor: 'blue',
+            tabBarInactiveTintColor: 'gray',
+          })}
+        >
+          <Tab.Screen name='Home' component={HomeScreen}/>
+          <Tab.Screen name='Settings' component={SettingsPage}/>
+        </Tab.Navigator>
     </NavigationContainer>
   );
 }
